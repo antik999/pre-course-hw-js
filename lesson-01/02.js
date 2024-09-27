94c9917c189670e67addd2f1d248a6aa91802845
antik999 <antikill-fc@yandex.ru>
2024-09-27 15:26:29 +0300

    add: lesson-01 task 02

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,9 @@
-// code
+let number1 = Math.floor(Math.random() * 100);
+let numberChetStatus;
+function numberChet () {    
+    if (number1 % 2 == 0) {numberChetStatus = ":Это четное число"}
+    else numberChetStatus = ":Это нечетное число";
+    return numberChetStatus;
+}
+numberChet ();
+console.log(number1 + numberChetStatus)
